cd8151a3cda71bebace79aca51267b78b284c12f
Daniel Barter <danielbarter>
2016-10-15 18:43:17 -0400

    fixed directory structure

---
+++ .emacs.d/customizations/editing.el
@@ -0,0 +1,7 @@
+;; Don't use hard tabs
+(setq-default indent-tabs-mode nil)
+
+;; Emacs can automatically create backup files. This tells Emacs to
+;; put all backups in ~/.emacs.d/backups. More info:
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))

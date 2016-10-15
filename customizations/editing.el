0d7e92c67602fb04bc176c565aaceb08cc215ffb
Daniel Barter <danielbarter>
2016-10-15 18:44:24 -0400

    woops

--- customizations/editing.el
+++
@@ -1,7 +0,0 @@
-;; Don't use hard tabs
-(setq-default indent-tabs-mode nil)
-
-;; Emacs can automatically create backup files. This tells Emacs to
-;; put all backups in ~/.emacs.d/backups. More info:
-(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                               "backups"))))

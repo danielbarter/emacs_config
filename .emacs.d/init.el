b29cd6a8a3285242d2d9cce5254fd72c33d3c786
Daniel Barter <danielbarter>
2017-01-19 16:30:53 -0500

    refactor a little

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,23 +9,10 @@
 ;;load package manager
 (package-initialize)
 
-;; Download the ELPA archive description if needed.
-;; This informs Emacs about the latest versions of all packages, and
-;; makes them available for download.
-(when (not package-archive-contents)
-  (package-refresh-contents))
 
-;;list of packages
-(defvar packages
-  '(
-    evil
-    rainbow-delimiters))
 
-;;if package is not downloaded, then grab it
-(dolist (p packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
+;; install packages
+(load "install.el")
 
 ;; These customizations change the way emacs looks and disable/enable
 ;; some user interface elements
